5ef30613ce902cff12fb1f3f8701ea65bc6018b9
njeriigitau <mitchelgitau@gmail.com>
2019-02-15 22:43:09 +0300

    Add scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,9 @@ alert ("This is an isosceles triangle!");
 else if((side1+side2)<=side3 || (side2+side3)<=side1 || (side1+side2)<=side3){
 alert ("This is not a triangle!");
 }
+else(){
+alert ("This is a scalene triangle!")
+}
+elseif((side1!=side2 && side2!=side3 && side3!=side1)
+alert ("This is a scalene triangle!")
+}
