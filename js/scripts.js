e0bf368c9ce5fffda11c6174f272d8a195b2e0c0
njeriigitau <mitchelgitau@gmail.com>
2019-02-17 15:21:54 +0300

    Add styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 function triangle(){
-  var side1 = parseInt(document.getElementById("side one").value);
-  var side2 = parseInt(document.getElementById("side two").value);
-  var side3 = parseInt(document.getElementById("side three").value);
+  var side1 = parseInt(document.getElementById("sideOne").value);
+  var side2 = parseInt(document.getElementById("sideTwo").value);
+  var side3 = parseInt(document.getElementById("sideThree").value);
 }
+
 if(side1 === side2 && side2 === side3){
 alert ("This is an equilateral triangle!");
 }
